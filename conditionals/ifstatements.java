20d249d084de97d43f1522d19c6914d31500b852
clencyc <clency2023@gmail.com>
2024-02-28 11:37:53 +0300

    fixed bugs

---
+++ conditionals/ifstatements.java
@@ -0,0 +1,15 @@
+package conditionals;
+
+public class ifstatements {
+        public static void main(String[] args) {
+        int x = 20;
+        int y = 18;
+        
+          if (x > y) {
+            System.out.println(x + " is the largest"); // obviously
+          } else {
+            System.out.println("x is less");
+
+        }
+      }
+  }
